43820a5a18abb0ebdf485aae71565298d33e8848
rsheroxd <67628920+rsheroxd@users.noreply.github.com>
2020-07-04 14:54:25 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,12 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1143081337:AAEYkVmfOeh0QP1Owpwb4-arQ2nCbovPP8E"
+  # These example values won't work. You must get your own app_id and
+  # api_hash from https://my.telegram.org, under API Development.
+  APP_ID = 928403
+  API_HASH = "4a546388b91f6f815c4a6adbbc30d574"
+  # please read https://t.me/c/1279877202/74
+  # if you have not read the above README
+  AUTH_CHANNEL = [-1001246022921]
+  DOWNLOAD_LOCATION = "home/ubuntu/Leech/senpaidls
