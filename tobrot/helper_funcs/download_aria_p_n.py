f86d90e2e928d84071a74c5a8f7168552c9f6b4f
rsheroxd <67628920+rsheroxd@users.noreply.github.com>
2020-07-08 17:02:03 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -301,10 +301,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 except:
                     pass
                 #
-                msg = f"\nDownloading File: `{downloading_dir_name}`"
-                msg += f"\nSpeed: {file.download_speed_string()} 🔽 / {file.upload_speed_string()} 🔼"
-                msg += f"\nProgress: {file.progress_string()}"
-                msg += f"\nTotal Size: {file.total_length_string()}"
+                
 
                 if is_file is None :
                    msg += f"\n<b>Connections:</b> {file.connections}"
@@ -312,11 +309,11 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                    msg += f"\n<b>Info:</b>[ P : {file.connections} || S : {file.num_seeders} ]"
 
                 # msg += f"\nStatus: {file.status}"
-                msg += f"\nETA: {file.eta_string()}"
-                msg += f"\n<code>/cancel {gid}</code>"
+                # msg += f"\nETA: {file.eta_string()}"
+                # msg += f"\n<code>/cancel {gid}</code>"
                 # LOGGER.info(msg)
                 if msg != previous_message:
-                    await event.edit(msg)
+                    # await event.edit(msg)
                     previous_message = msg
             else:
                 msg = file.error_message
